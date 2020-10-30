004c05979df1a7042c6454c56b7ec2bd7c5757f8
kmullen444 <kmullen444@gmail.com>
2020-10-30 11:58:37 -0700

    Added a tracker for rounds played and a reset button, also a few visual updates.

--- RSPv2.js
+++ RSPv2.js
@@ -1,65 +1,86 @@
 let wins = 0;
 let loses = 0;
+let gameRounds = wins + loses;
 let playerChoice;
 let compChoice;
 
+const pChoice = document.querySelectorAll('.choice');
+const gameResults = document.querySelector('#gameResults');
+const loseRound = document.querySelector('#lose');
+const winRound = document.querySelector('#win');
+const rounds = document.querySelector('#rounds');
+const resetBtn = document.querySelector('#reset');
 
-
-function player(){
-	const pChoice = document.querySelectorAll('.choice');
-		pChoice.forEach(pChoice => pChoice.addEventListener('click',() => {
-			playerChoice = pChoice.id;
-			console.log(playerChoice);
-			computer();
-			playRound(playerChoice,compChoice);
-		}));
-};
-
-function computer(){
-	const choices = ['Rock','Paper','Scissors'];
+function computer() {
+	const choices = ['Rock', 'Paper', 'Scissors'];
 	let rand = Math.floor(Math.random() * choices.length);
 	compChoice = choices[rand];
 	console.log(compChoice + '2');
 	return compChoice;
 }
 
-function winGame (playerChoice,compChoice){
-	const winRound = document.querySelector('.win');
+function winGame(playerChoice, compChoice) {
 	winRound.textContent = `Wins: ${wins}`;
-	const win = document.querySelector('.gameResults');
-	win.textContent = `You win! ${playerChoice} beats ${compChoice}!`;
-}
- 
-function loseGame (playerChoice,compChoice){
-	const loseRound = document.querySelector('.lose');
-	loseRound.textContent = `Loses: ${loses}`;
-	const lose = document.querySelector('.gameResults');
-	lose.textContent = `You lose! ${compChoice} beats ${playerChoice}!`
+	gameResults.textContent = `You win! ${playerChoice} beats ${compChoice}!`;
 }
 
-function tieGame (){
-	const tie = document.querySelector('.gameResults');
-	tie.textContent = `Its a tie! Pick Again!`
+function loseGame(playerChoice, compChoice) {
+	loseRound.textContent = `Loses: ${loses}`
+	gameResults.textContent = `You lose! ${compChoice} beats ${playerChoice}!`
 }
 
-function playRound(playerChoice,compChoice){
-	if(playerChoice === 'Rock' && compChoice === `Scissors` || playerChoice === `Paper` && 
-		compChoice === `Rock` || playerChoice === `Scissors` && compChoice === `Paper`){
-			wins++;
-			winGame(playerChoice,compChoice);
-	}else if(playerChoice === `Rock` && compChoice === `Paper` || playerChoice === `Paper` && 
-		compChoice === `Scissors` || playerChoice === `Scissors` && compChoice === `Rock`){
-			loses++;
-			loseGame(playerChoice,compChoice);
-	}else{
-		tieGame();
+function playRound(playerChoice, compChoice) {
+	if (playerChoice === 'Rock' && compChoice === `Scissors` || playerChoice === `Paper` &&
+		compChoice === `Rock` || playerChoice === `Scissors` && compChoice === `Paper`) {
+		wins++;
+		gameRounds++;
+		winGame(playerChoice, compChoice);
+	} else if (playerChoice === `Rock` && compChoice === `Paper` || playerChoice === `Paper` &&
+		compChoice === `Scissors` || playerChoice === `Scissors` && compChoice === `Rock`) {
+		loses++;
+		gameRounds++;
+		loseGame(playerChoice, compChoice);
+	} else {
+		gameResults.textContent = `It's a tie! Pick Again!`;
 	}
 
 }
 
+function playedRounds() {
+	rounds.textContent = `Rounds: ${gameRounds}`;
+	if (gameRounds === 5) {
+		if (wins > loses) {
+			gameResults.textContent = `You win! Final Score: Wins: ${wins}, Loses: ${loses}. Click Restart to play again`;
+		} else {
+			gameResults.textContent = `You lose! Final Score: Wins: ${wins}, Loses: ${loses}. Click Restart to play again`;
+		}
+	}
+}
 
-const startGame = document.querySelectorAll('.game');
-startGame.forEach(startGame => startGame.addEventListener('click', player()));
+resetBtn.addEventListener('click', () => {
+	wins = 0;
+	loses = 0;
+	gameRounds = 0;
+	winRound.textContent = `Wins: ${wins}`;
+	loseRound.textContent = `Loses: ${loses}`;
+	rounds.textContent = `Rounds: ${gameRounds}`;
+	gameResults.textContent = '';
+});
 
 
+pChoice.forEach(pChoice => pChoice.addEventListener('click', () => {
+	playerChoice = pChoice.id;
+	console.log(playerChoice);
+	computer();
+	playRound(playerChoice, compChoice);
+	playedRounds();
+}));
+
+
+
+winRound.textContent = `Wins: ${wins}`;
+
+loseRound.textContent = `Loses: ${loses}`
+
+rounds.textContent = `Rounds: ${gameRounds}`;
 
